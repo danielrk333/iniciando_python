46d1805efa86dd64c339b3a05275a5c31ac3da95
Daniel Lavorini <daniel.lavorini333@gmail.com>
2024-02-29 17:31:03 -0300

    inclusion of files date 2024.02.29.1730

---
+++ curso_guanabara/aula8/aula8.py
@@ -0,0 +1,14 @@
+# from math import sqrt
+# num = int(input('Digite um número: '))
+# raiz = sqrt(num)
+# print('A raiz quadrada do número {} é {:.3f}'.format(num, raiz))
+##################################################################
+
+# import random
+
+# num = random.randint(0,987)
+# print(num)
+#################################
+
+import emoji
+print(emoji.emojize('Olá, Mundo :globe_showing_americas:', language='alias'))
